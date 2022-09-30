ddfb04b7d9375db29460155928407ebb1dcb74cd
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:24:31 +0100

    feat: Add enums
    MPM-3

---
+++ BankingAppTask/BankingAppTask/Enums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask
+{
+    public class Enums
+    {
+        public enum Menu
+        {
+            [Description("Balance")]
+            Balance = 1,
+
+            [Description("Deposit")]
+            Deposit = 2,
+
+            [Description("Withdraw")]
+            Withdraw = 3,
+
+            [Description("Transfer")]
+            Transfer = 4,
+
+            [Description("Logout")]
+            Logout = 5,
+        }
+
+        public enum TypeOfTransaction
+        {
+            [Description("Deposit")]
+            Deposit = 1,
+
+            [Description("Withdraw")]
+            Withdraw = 1,
+
+            [Description("Transfer")]
+            Transfer = 1,
+        }
+    }
+}
