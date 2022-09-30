b7babd6e11f8d0bdb8ffc1bd3a30337671514570
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:23:33 +0100

    feat: Add IWithdraw
    MPM-2

---
+++ BankingAppTask/BankingAppTask/Interfaces/IWithdraw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask.Interfaces
+{
+    public interface IWithdraw
+    {
+        void Withdraw(Accounts account);
+    }
+}
