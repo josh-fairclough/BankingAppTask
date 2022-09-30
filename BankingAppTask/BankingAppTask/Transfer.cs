a25c82bd2cf29b2a8f5d6a6949a83585ac923085
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:24:57 +0100

    feat: add transfer
    MPM-3

---
+++ BankingAppTask/BankingAppTask/Transfer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask
+{
+    public class Transfer
+    {
+        public decimal TransferAmount { get; set; }
+        public Int64 AccountReceiving { get; set; }
+    }
+}
