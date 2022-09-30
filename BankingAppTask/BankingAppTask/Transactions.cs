53abc2a40ed6e64b32dad99b48a343c08031aa0f
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:24:44 +0100

    feat: add transactions
    MPM-3

---
+++ BankingAppTask/BankingAppTask/Transactions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask
+{
+    public class Transactions
+    {
+        public Enums.TypeOfTransaction TransactionType { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public Int64 AccountTo { get; set; }
+        public Int64 AccountFrom { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
