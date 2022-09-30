8cccb145724a4d8c52ccc79ec470a843419dd2a2
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:25:07 +0100

    feat: add accounts
    MPM-3

---
+++ BankingAppTask/BankingAppTask/Accounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask
+{
+    public class Accounts
+    {
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int AccountNumber { get; set; }
+        public int Pin { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
