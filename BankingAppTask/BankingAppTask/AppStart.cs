573194e0035ffa4f1fb91e405c2440986c432201
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 11:20:25 +0100

    feat: add AppStart
    MPM-1

---
+++ BankingAppTask/BankingAppTask/AppStart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppTask
+{
+    class AppStart
+    {
+        static void Main(string[] args)
+        {
+            BankingAppManager initialise = new BankingAppManager();
+            initialise.MockAccounts();
+            initialise.StartUp();
+        }
+    }
+}
