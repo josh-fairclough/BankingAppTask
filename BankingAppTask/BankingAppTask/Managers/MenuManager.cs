6309c66c37f4b21080a5cc37b23f32166fe4056b
Joshua Fairclough <joshua.fairclough@iress.com>
2022-09-30 12:20:31 +0100

    fix: remove and sort usings
    MPM-3

--- BankingAppTask/BankingAppTask/Managers/MenuManager.cs
+++ BankingAppTask/BankingAppTask/Managers/MenuManager.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Services;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankingAppTask.Managers
 {
